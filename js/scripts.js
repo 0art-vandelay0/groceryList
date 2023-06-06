74394730bda6848e4cdb18194753857a9703bd82
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-06 10:28:08 -0700

    Add content Fix css bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 function handleForm(event) {
     event.preventDefault();
 
-    
+    const userGroceries = document.querySelectorAll("input[name='groceries']");
+    const userGroceriesArray = Array.from(userGroceries);
+
+    const resulth2 = document.createElement("h2");
+    resulth2.append("Your Groceries List");
+    document.body.append(resulth2);
+
+    userGroceriesArray.forEach(function(grocery) {
+        newUl = document.createElement("ul");
+        newLi = document.createElement("li");
+        newLi.append(grocery.value);
+        newUl.append(newLi);
+        document.body.append(newUl);
+    });
+
+}
+
+function hideForm() {
+    document.getElementById("groceriesList").setAttribute("class", "hidden");
 }
 
 window.addEventListener('load', function() {
-    document.getElementById("form#groceries").addEventListener("submit", handleForm);
+    document.getElementById("groceriesList").addEventListener("submit", handleForm);
+    document.getElementById("groceriesList").addEventListener("submit", hideForm);
 });
